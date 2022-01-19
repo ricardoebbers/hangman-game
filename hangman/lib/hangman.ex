c475a3fff4fc5f31ecc75550f0efdfec4967d04a
Ricardo Leão <ricardo@jaya.tech>
2022-01-19 14:06:30 -0300

    Add types and static type checking

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,17 +1,18 @@
 defmodule Hangman do
+  alias Hangman.Impl.Game
+
   @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
 
-  @type game :: any
+  @opaque game :: Game.t()
   @type tally :: %{
           turns_left: integer,
           game_state: state,
           letters: list(String.t()),
-          used: list(String.t())
+          used: MapSet.t(String.t())
         }
 
   @spec new_game() :: game
-  def new_game do
-  end
+  defdelegate new_game, to: Game
 
   @spec make_move(game, String.t()) :: {game, tally}
   def make_move(game, guess) do
